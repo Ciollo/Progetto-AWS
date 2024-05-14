c4f4af7e121808d422fdbe491fbe2267a10d5e64
Ubuntu <ubuntu@ip-172-31-24-5.ec2.internal>
2024-05-14 15:40:47 +0000

    idk

--- src/index.php
+++ src/index.php
@@ -1,12 +1,15 @@
-<!doctype html>
+<!Doctype html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
   </head>
+<!--
   <body onload="window.location = 'src/AWS_website/public/login.html'">
-    <!-- redirect alla main,html -->
-    <!-- (preferisco mettere tutti i contenuti pubblici agli utenti nella cartella /public) -->
+     redirect alla main,html 
+     (preferisco mettere tutti i contenuti pubblici agli utenti nella cartella /public) 
+-->
+ciao
   </body>
 </html>
